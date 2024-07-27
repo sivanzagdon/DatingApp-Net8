c54121bae81e9033e72e3a4e607138094328e6a2
sivanzagdon <sivan0252@gmail.com>
2024-07-28 01:07:29 +0300

    End of section 5

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, inject, Input, input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  private accountService=inject(AccountService);
+  @Input() usersFromHomeComponent: any;
+  @Output() cancelRegister= new EventEmitter();
+  model: any={}
+
+  register(){
+    this.accountService.register(this.model).subscribe({
+      next: response=>{
+        console.log(response);
+        this.cancel();
+      },
+      error: error=> console.log(error)
+    })
+  }
+
+  cancel(){
+    this.cancelRegister.emit(false);
+  }
+
+}
